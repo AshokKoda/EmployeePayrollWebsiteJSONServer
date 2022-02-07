cd4e64479fa797373206bd21b525eda7d3c8c33c
Ashok Koda <a.koda59@gmail.com>
2022-02-07 10:30:37 +0530

    Added:Adding data into JSON Server

--- js/form.js
+++ js/form.js
@@ -50,3 +50,72 @@ function checkDate() {
         dateError.textContent = e;
     }
 }
+
+function redirect() {
+    console.log("redirect")
+    //resetForm();
+    window.location.replace(site_properties.home_page)
+}
+
+const save = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    try {
+        setEmployeePayrollObject();
+        if(site_properties.use_local_storage.match("true")){
+            createAndUpdateStorage();
+            alert("Data Stored With Name "+employPayrollObject._name);
+            redirect();
+        } else {
+            alert("Data Stored With Name "+employPayrollObject._name);
+            redirect();
+            createOrUpdateEmployeeInJsonServer();
+        }
+    } catch (e) {
+        console.log(e)
+        return;
+    }
+}
+
+const setEmployeePayrollObject = () => {
+    employPayrollObject._name = getInputValueId('#name');
+    employPayrollObject._profilePic = getSelectedValue('[name=profile]').pop();
+    employPayrollObject._gender = getSelectedValue('[name=gender]').pop();
+    employPayrollObject._department = getSelectedValue('[name=department]');
+    employPayrollObject._salary = getInputValueId('#salary');
+    employPayrollObject._note = getInputValueId('#notes').replace(/\s/g, ' ');
+    let date = getInputValueId('#day') + " " + getInputValueId('#month') + " " + getInputValueId('#year');
+    employPayrollObject._startDate = Date.parse(date);
+}
+
+const getInputValueId = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getSelectedValue = (propertyValue) => {
+    let allItem = document.querySelectorAll(propertyValue);
+    let setItem = [];
+    allItem.forEach(item => {
+        if (item.checked) {
+            setItem.push(item.value);
+        }
+    });
+    return setItem;
+}
+
+function createOrUpdateEmployeeInJsonServer() {
+    let url=site_properties.server_url;
+    let methodCall="POST";
+    let message="Data Store with name ";
+    
+    makeServiceCall(methodCall,url,true,employPayrollObject)
+        .then(response=>{
+             alert(message +employPayrollObject._name)
+             redirect();
+        })
+        .catch(error=>{
+            console.log("inside error")
+            throw error
+        });
+}
